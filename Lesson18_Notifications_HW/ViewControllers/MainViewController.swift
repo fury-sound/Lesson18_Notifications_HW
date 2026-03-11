f16600e2aa0591825a4479442ad6380b8ce150d6
Valery Zvonarev <furysound1@yandex.ru>
2026-03-11 22:33:39 +0300

    Удален закомментированный код. Добавлен clearButtonMode в textField. Клавиатура исчезает при нажатии на Enter и тапе в пустом пространстве вью

--- Lesson18_Notifications_HW/ViewControllers/MainViewController.swift
+++ Lesson18_Notifications_HW/ViewControllers/MainViewController.swift
@@ -32,6 +32,7 @@ final class MainViewController: UIViewController {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.layer.borderWidth = 2
         textField.layer.cornerRadius = 12
+        textField.clearButtonMode = .whileEditing
         return textField
     }()
 
@@ -80,7 +81,6 @@ final class MainViewController: UIViewController {
 
     private func setupSubviews() {
         myTextField.delegate = self
-//        myTextField.becomeFirstResponder()
         myTextField.addTarget(self, action: #selector(textFieldTapped), for: .editingDidBegin)
         myButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         [myLabel, myTextField, myButton].forEach {
@@ -100,7 +100,6 @@ final class MainViewController: UIViewController {
             myTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
             myTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
             myTextField.heightAnchor.constraint(equalToConstant: 50),
-            //            myButton.topAnchor.constraint(equalTo: myTextField.bottomAnchor, constant: 100),
             myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             myButton.heightAnchor.constraint(equalToConstant: 50),
             myButton.widthAnchor.constraint(equalToConstant: 150),
@@ -109,51 +108,25 @@ final class MainViewController: UIViewController {
     }
 
     @objc private func didShowKeyboard(_ notification: Notification) {
-        //        print("keyboard is showing")
         guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-        //        print(keyboardFrame.height, keyboardFrame.width)
         let bottomInset = keyboardFrame.height - view.safeAreaInsets.bottom + 10
         buttonBottom?.constant = -bottomInset
-        //        let buttonTop = bottomInset + 50 + 20
         let buttonTopY = view.bounds.height - bottomInset - 50
-        //        print(view.bounds.height, view.center.y, myTextField.frame.maxY, myButton.frame.minY, bottomInset, buttonTopY)
-        //        if (view.center.y + 25) > buttonTopConstant {
         if (myTextField.frame.maxY + 25) > buttonTopY {
             let newYValue = buttonTopY - view.center.y + 25
             textFieldYValue?.constant = -newYValue
-            //            print("pinch", myTextField.frame.maxY)
         }
-
-        //        UIView.animate(withDuration: 1.5, delay: 0, options: [.curveEaseOut]) {
         UIView.animate() {
             self.view.layoutIfNeeded()
-            //            print("myButton.frame.minY", self.myButton.frame.minY)
         }
     }
 
     @objc private func willHideKeyboard(_ notification: Notification) {
-        print("keyboard is hiding")
-        //        let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 1.25
-        //        let curveValue = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int ?? 0
-        ////        let animationCurve = UIView.AnimationOptions(rawValue: curveValue << 16)
-        //        let animationOptions = UIView.AnimationOptions(rawValue: UInt(curveValue))
-
         self.buttonBottom?.constant = -50
         self.textFieldYValue?.constant = 0
-
-        //        UIView.performWithoutAnimation {
-        //            self.view.layoutIfNeeded()
-        //        }
-
-        //        UIView.animate(withDuration: duration, delay: 0, options: animationOptions) {
         UIView.animate() {
             self.view.layoutIfNeeded()
         }
-        //        UIView.animate(withDuration: 2.5, delay: 0, options: [.curveEaseOut]) {
-        //            self.buttonBottom?.constant = -50
-        //            self.textFieldYValue?.constant = 0
-        //            self.view.layoutIfNeeded()
-        //        }
     }
 
     @objc private func textFieldTapped() {
@@ -161,9 +134,7 @@ final class MainViewController: UIViewController {
     }
 
     @objc private func didSingleTap(_ gesture: UITapGestureRecognizer) {
-        //        UIView.animate(withDuration: 1.0, delay: 0, options: [.curveEaseInOut]) {
         self.myTextField.resignFirstResponder()
-        //        }
     }
 
     @objc private func didTapButton() {
@@ -173,23 +144,12 @@ final class MainViewController: UIViewController {
         } else {
             myText = "No text"
         }
-        print(myText)
         myLabel.text = myText
     }
 }
 
 extension MainViewController: UITextFieldDelegate {
-    //    func textFieldDidEndEditing(_ textField: UITextField) {
-    //        print("editing completed")
-    //    }
-
-//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-//        textField.becomeFirstResponder()
-//        return true
-//    }
-
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("in textFieldShouldReturn")
         myTextField.resignFirstResponder()
         return true
     }
